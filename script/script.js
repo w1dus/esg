840f4accdfbe764b94a309a898a6d86b45cd08f9
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-01-06 16:55:58 +0900

    헤더, 푸터

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    rightMenuHandler();
 })
 
+const rightMenuHandler = () => {
+    $('header .mainSection .menuBtn').click(function(){
+        $('.menuCloseBg').addClass('show');
+        $('.rightMenuSection').addClass('show')
+    })
+    $('.menuCloseBg').click(function(){
+        $('.menuCloseBg').removeClass('show');
+        $('.rightMenuSection').removeClass('show')
+    })
+    $('.rightMenuSection .btnWrap .closeBtn').click(function(){
+        $('.menuCloseBg').removeClass('show');
+        $('.rightMenuSection').removeClass('show')
+    })
 
+    $('.rightMenuSection .menuList .subMenuBtn').click(function(e){
+        e.preventDefault();
+        $(this).siblings('.subMenuList').slideToggle();
+        $(this).toggleClass('active');
+    })
+}
