e1f7789b4e5c13fb8ba6f8444efac470251fb8fd
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-01-07 15:42:17 +0900

    메인 퍼블리싱 완료

--- script/script.js
+++ script/script.js
@@ -2,7 +2,42 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
     rightMenuHandler();
+    mainPartnerSlide();
+    privayPopup();
 })
+
+const privayPopup = () => {
+    /* privacy popup */
+    $('.privacy_view').click(function(e){
+        e.preventDefault();
+        $('.f_popup_bg').addClass('on');
+    })
+    $('.f_popup_bg').click(function(){
+        $('.f_popup_bg').removeClass('on');
+    })
+    $('.f_popup_bg .content_wrap').click(function(e){
+        e.stopPropagation();
+    })
+}
+
+const mainPartnerSlide = () => {
+    var swiper = new Swiper(".main .partnerSection .slideWrap .mySwiper", {
+        loop : true,
+        spaceBetween: 10,
+        loopedSlides: 1,
+        observer: true,
+        observeParents: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: ".main .partnerSection .slideWrap .pnBtnWrap .nextBtn",
+          prevEl: ".main .partnerSection .slideWrap .pnBtnWrap .prevBtn",
+        },
+    });
+}
+
 
 const rightMenuHandler = () => {
     $('header .mainSection .menuBtn').click(function(){
